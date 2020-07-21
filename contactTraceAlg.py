0da9f73f31fd377682ec896b43b9218bb78ec8ed
Joey Clary <jclary1603@gmail.com>
2020-07-21 11:55:33 -0500

    Completed Primary Contact Alg

--- contactTraceAlg.py
+++ contactTraceAlg.py
@@ -1,11 +1,13 @@
 class TraceAlg:
     def __init__(self, studentDataFileName):
-        self.studentDataFile = open(studentDataFileName, 'r')
+        self.studentDataFileName = studentDataFileName
+        self.classDict = self.getClassDict()
 
-    def getData(self):
+    def getClassDict(self):
         classDict = {} #ClassName: studName, studName, studName...
+        studentDataFile = open(studentDataFileName, 'r')
 
-        for line in self.studentDataFile:
+        for line in studentDataFile:
             studInfo = line.split('\t') #[studName, Hour 1, Hour 2, Hour 3, Hour 4, Hour 5, Hour 6, Hour 7]
             studName = studInfo[0]
 
@@ -20,11 +22,41 @@ class TraceAlg:
                 classDict[studInfo[hourNum] + str(hourNum)] = classStudents
 
         return classDict
+    
+    def getClassRoster(self, chkClass):
+        classRoster = self.classDict[chkClass]
+        return classRoster
 
+    def getStudentSchedule(self, chkName):
+        studentDataFile = open(studentDataFileName, 'r')
+
+        for line in studentDataFile:
+            studInfo = line.split('\t')
+            studName = studInfo[0]
+            if studName == chkName:
+                return studInfo
+
+    def getPrimaryContacts(self, chkName):
+        studInfo = self.getStudentSchedule(chkName)
+        del(studInfo[0])
+        del(studInfo[7])
+        hourNum = 1
+        primaryContacts = {}
+
+        for classTeach in studInfo:
+            chkClass = classTeach + str(hourNum)
+            classRoster = self.getClassRoster(chkClass)
+            classRoster.remove(chkName)
+            for stud in classRoster:
+                try:
+                    primaryContacts[stud].append(chkClass)
+                except(KeyError):
+                    primaryContacts[stud] = [chkClass]
+            hourNum += 1
+        return primaryContacts
     def main(self):
-        classDict = self.getData()
-        for className in classDict:
-            print(classDict[className])
+        primaryContacts = self.getPrimaryContacts('Student1')
+        print(primaryContacts)
 
 if __name__ == "__main__":
     studentDataFileName = 'C:\\Users\\jclar\\Desktop\\School\\Senior\\School-Contact-Tracing\\demoSchedule.txt'
